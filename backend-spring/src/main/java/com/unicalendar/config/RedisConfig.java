5d41f53b421e75c90ff38a4a2b5fc3a3a6b6143b
smietanka4 <wloczewskikarol5@gmail.com>
2026-07-27 21:49:15 +0200

    poprawione testy na ci/cd backendu

--- backend-spring/src/main/java/com/unicalendar/config/RedisConfig.java
+++ backend-spring/src/main/java/com/unicalendar/config/RedisConfig.java
@@ -3,6 +3,7 @@ package com.unicalendar.config;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -13,9 +14,11 @@ import java.time.Duration;
 
 /**
  * Redis cache – odpowiednik Django django-redis z TIMEOUT=300.
+ * Wyłączony w profilu testowym (testy używają spring.cache.type=none).
  */
 @Configuration
 @EnableCaching
+@Profile("!test")
 public class RedisConfig {
 
     @Bean
